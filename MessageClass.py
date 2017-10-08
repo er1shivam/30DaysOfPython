bf266986f220e5590c9376d5c4236fbf75a2a103
er1shivam <er1shivam@gmail.com>
2017-10-08 14:10:38 +0530

    modified to send email to list of users

--- MessageClass.py
+++ MessageClass.py
@@ -1,4 +1,15 @@
 import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+host = "smtp.gmail.com"
+port = 587
+username = "er05shivam@gmail.com"
+from_email = username
+password = "Ilove<3mymom5."
+to_email = "er1shivam@gmail.com"
+
 
 class MessageUser():
     user_details = []
@@ -12,9 +23,10 @@ class MessageUser():
 
     Team CodeForLife
     """
+    email_message = []
     def add_user(self,name,amount,email=None):
         name = name[0].upper() + name[1:].lower()
-        amount = "%.2f".%(amount)
+        amount = "%.2f"%(amount)
         detail = {
             "name": name,
             "amount": amount,
@@ -41,15 +53,52 @@ class MessageUser():
                     date=date,
                     total=amount
                 )
-                self.message.append(new_msg)
+                user_mail = detail.get("email")
+                if user_mail:
+                    user_data = {
+                        "email": user_mail,
+                        "message": new_msg
+                    }
+                    self.email_message.append(user_data)
+                else:
+                    self.message.append(new_msg)
             return self.message
         return []
 
+    def send_message(self):
+        self.make_message()
+        if len(self.email_message) > 0:
+            for detail in self.email_message:
+                user_email = detail["email"]
+                user_message = detail["message"]
 
+                try:
+                    email_conn = smtplib.SMTP(host,port)
+                    email_conn.ehlo()
+                    email_conn.starttls()
+                    email_conn.login(username,password)
+
+                    the_msg = MIMEMultipart("alternative")
+                    the_msg['Subject'] = "Hello there!"
+                    the_msg["From"] = from_email
+                    the_msg["To"] = user_email
+                    part_1 = MIMEText(user_message,'plain')
+                    the_msg.attach(part_1)
+
+                    email_conn.sendmail(from_email,[user_email], the_msg.as_string())
+                    email_conn.quit()
+                except smtplib.SMTPException:
+                    print("Error sending message")
+            return True
+        return False
+
+    def premailmsg(self):
+        for i in self.email_message:
+            print(i)
 
 obj = MessageUser()
-obj.add_user("shivam",455.5)
-obj.add_user("Rahul",125.34)
+obj.add_user("Shivam!",455.5,email='er3shivam@gmail.com')
+obj.add_user("Shivam2",125.34,email='er1shivam@gmail.com')
 
 print(obj.get_details())
-print(obj.make_message())
+print(obj.send_message())
